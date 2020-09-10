682013e5c0fa8080c2f55d92e59c41a2f3f74287
Софья Курбатова <sofiakurbatova@outlook.com>
2020-09-10 22:39:00 +0300

    Рабочая статическая библиотека, которая выводит данные о системе.

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,14 +1,14 @@
-//#pragma comment (lib,"STATILIBR.lib")
 #include <Windows.h>
 #include <wchar.h>
 #include <locale.h>
 #include <stdio.h>
-#include <STATILIBR.h>
+#include <C:\Users\Kurbatova\source\LW2020\lw_os_1\STATILIBR\STATILIBR.h>
 
 int wmain()
 {
 	_wsetlocale(LC_ALL, TEXT(""));
-	wprintf(TEXT("¬ывод данных статической библиотеки\n"));
+	wprintf(TEXT("¬ывод данных статической библиотеки"));
+	//_wsetlocale(LC_ALL, );
 	PrintCompName();
 	PrintDNSName();
 	PrintUserName();
