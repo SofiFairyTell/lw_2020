3366f6e846df0f4efc38dced9660e2214e9bbd2b
Софья Курбатова <sofiakurbatova@outlook.com>
2020-09-12 01:26:03 +0300

    Функция для определения операционной системы

--- DYNLIB1/DYNLIB1.cpp
+++ DYNLIB1/DYNLIB1.cpp
@@ -4,6 +4,8 @@
 #include <ShlObj.h>
 #include "DYNLIB1.h"
 
+#pragma warning(disable : 4996) //отключает Ошибку deprecate. Возникает, когда используется устаревшая функция
+
 BOOL WINAPI DllMain(HINSTANCE hinstDll,DWORD idReason, LPVOID lpReserved )
 {
 	return TRUE;		
@@ -17,5 +19,64 @@ DYNLIB1_API void PrintSysDir(const long csidl[], unsigned long nCount)
 		if (S_OK == hr)
 			wprintf(TEXT("%d:%s\n"), i + 1, szPath);
 	}
-		return;
+			return;
+}
+DYNLIB1_API void PrintOSinfo()
+{
+	OSVERSIONINFO osver;
+	osver.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
+	BOOL ret = GetVersionEx(&osver);
+	if (ret != FALSE)
+	{
+		wprintf(TEXT("Version:%d.%d.%d\n", osver.dwBuildNumber, osver.dwMajorVersion, osver.dwMinorVersion));
+		switch (osver.dwMajorVersion)
+		{
+		case 5:
+		{
+			switch (osver.dwMinorVersion)
+			{
+			case 1:
+				wprintf(TEXT("Windows XP"));
+				break;
+			case 2:
+				wprintf(TEXT("Windows 2003"));
+				break;
+			}
+		}
+		break;
+		case 6:
+		{
+			switch (osver.dwMinorVersion)
+			{
+			case 0:
+				wprintf(TEXT("Windows Vista"));
+				break;
+			case 1:
+				wprintf(TEXT("Windows 7"));
+				break;
+			case 2:
+				wprintf(TEXT("Windows 8"));
+				break;
+			case 3:
+				wprintf(TEXT("Windows 8.1"));
+				break;
+			}
+		}
+			break;
+		case 10:
+			switch (osver.dwMinorVersion)
+			{
+			case 0:
+				{
+				wprintf(TEXT("WINDOWS 10"));
+				}
+				break;	 
+			}
+		
+		default:
+			wprintf(TEXT("OS undefined"));
+			break;
+		}
+
+	}
 }
