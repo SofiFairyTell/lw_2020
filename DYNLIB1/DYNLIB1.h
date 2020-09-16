341021072b2bfa79b95aad50657f53437b3d64e8
Софья Курбатова <sofiakurbatova@outlook.com>
2020-09-16 18:20:58 +0300

    Собрана библиотека DYNLIB3 отвечающая за вывод в стандартный поток значений системных параметров SPI_GETWORKAREA SPI_GETMOUSEWHEELROUTING

--- DYNLIB1/DYNLIB1.h
+++ DYNLIB1/DYNLIB1.h
@@ -2,9 +2,9 @@
 #if !defined(_DYN_LIB_1_H_)
 	#define _DYN_LIB_1_H_
 	#ifdef DYNLIB1_EXPORTS
-		#define DYNLIB1_API _declspec(dllexport)
+		#define DYNLIB1_API __declspec(dllexport)
 	#else
-		#define DYNLIB1_API _declspec(dllimport)
+		#define DYNLIB1_API __declspec(dllimport)
 	#endif // 
 #endif // !!defined(_DYN_LIB_1_H)
 #ifdef __cplusplus
