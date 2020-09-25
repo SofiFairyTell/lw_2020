0fcc3ede94b3609f08453670a74953bc2955a4e6
Софья Курбатова <sofiakurbatova@outlook.com>
2020-09-25 10:26:42 +0300

    add new resources

---
+++ lw_os_2/resource.h
@@ -0,0 +1,30 @@
+﻿//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется lw_os_21.rc
+//
+#define IDD_DIALOG1                     101
+#define IDR_MENU1                       102
+#define IDR_ACCELERATOR1                104
+#define IDC_EDIT1                       1001
+#define IDC_MONTHCALENDAR1              1002
+#define IDC_LIST1                       1003
+#define IDC_SPIN1                       1004
+#define ID_NEW_RECORD                   40001
+#define ID_NEW_RECORD2                  40002
+#define ID_DEL_RECORD                   40003
+#define ID_FIND_RECORD                  40004
+#define ID_FORMAT_FONT                  40005
+#define ID_40010                        40010
+#define ID_40011                        40011
+#define ID_EDIT                         40012
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_COMMAND_VALUE         40013
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
