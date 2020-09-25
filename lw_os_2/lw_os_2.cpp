b1c6acdeaf86c84b1bfe12caf86611a3d0d81ac5
Софья Курбатова <sofiakurbatova@outlook.com>
2020-09-25 11:00:18 +0300

    Есть настроенная таблица быстрых клавиш. Начато написание кода по созданию окна.

--- lw_os_2/lw_os_2.cpp
+++ lw_os_2/lw_os_2.cpp
@@ -1,1 +1,12 @@
 #include <Windows.h>
+#include <WindowsX.h>
+#include <CommCtrl.h>
+#include <tchar.h>
+
+#include "resource.h"
+//#include "afxres.h"
+
+#define WM_ADDITEM WM_USER+1
+
+HWND hWnd = NULL; //дескриптор окна
+HACCEL hAccel = NULL; //дескриптор акселератора 
