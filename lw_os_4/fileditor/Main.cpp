894cfafc3308275f671147efc17779cc3248aee9
Софья Курбатова <sofiakurbatova@outlook.com>
2020-11-03 23:56:38 +0300

    new prj WIN32API

---
+++ lw_os_4/fileditor/Main.cpp
@@ -0,0 +1,16 @@
+#include <Windows.h>
+#include <WindowsX.h>
+#include <tchar.h>
+#include <strsafe.h>
+
+#include "resource.h"
+
+
+#define IDC_EDIT_TEXT        2001
+
+#pragma comment(linker,"\"/manifestdependency:type                  = 'win32' \
+                                              name                  = 'Microsoft.Windows.Common-Controls' \
+                                              version               = '6.0.0.0' \
+                                              processorArchitecture = '*' \
+                                              publicKeyToken        = '6595b64144ccf1df' \
+                                              language              = '*'\"")
