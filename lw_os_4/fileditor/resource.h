22f45b93dcb4b38fa525ac2c3113055d70960f8a
Софья Курбатова <sofiakurbatova@outlook.com>
2020-11-04 01:48:04 +0300

    Этот проект собран.  Файл сохраняет, редактирует. Но из всех возможностей работы со шрифтом только bold выделение

--- lw_os_4/fileditor/resource.h
+++ lw_os_4/fileditor/resource.h
@@ -3,6 +3,7 @@
 // Используется fileditor.rc
 //
 #define IDR_MENU1                       101
+#define IDR_ACCELERATOR1                102
 #define ID_NEW_FILE                     40001
 #define ID_OPEN                         40002
 #define ID_40003                        40003
@@ -11,13 +12,14 @@
 #define ID_EXIT                         40006
 #define ID_SELECT_ALL                   40007
 #define ID_UNDO                         40008
+#define ID_FONT_EDIT                    40015
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
-#define _APS_NEXT_COMMAND_VALUE         40009
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40016
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
