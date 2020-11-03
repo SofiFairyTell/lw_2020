590be235b9f9072de06821a138aa65e378088c6c
Софья Курбатова <sofiakurbatova@outlook.com>
2020-11-03 23:56:38 +0300

    new prj WIN32API

---
+++ lw_os_4/fileditor/resource.h
@@ -0,0 +1,24 @@
+//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется fileditor.rc
+//
+#define IDR_MENU1                       101
+#define ID_NEW_FILE                     40001
+#define ID_OPEN                         40002
+#define ID_40003                        40003
+#define ID_SAVE                         40004
+#define ID_SAVE_AS                      40005
+#define ID_EXIT                         40006
+#define ID_SELECT_ALL                   40007
+#define ID_UNDO                         40008
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40009
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
