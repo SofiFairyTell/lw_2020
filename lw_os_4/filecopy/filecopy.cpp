16cdf8254cbe634a2658fca9872029d532954360
Софья Курбатова <sofiakurbatova@outlook.com>
2020-11-21 01:42:20 +0300

    Try made copy file in dialogbox

--- lw_os_4/filecopy/filecopy.cpp
+++ lw_os_4/filecopy/filecopy.cpp
@@ -7,13 +7,14 @@
 #include <tchar.h>
 #include <CommCtrl.h>
 #include <strsafe.h>
-
+#include <wchar.h>
 #include <shlobj.h>
 #include <shlwapi.h>
 #include <shobjidl.h>//for browserinfo
 #include <fileapi.h>
 #include <string>
-#include <wchar.h>
+
+#include "resource.h"
 
 using namespace std;
 #pragma warning(disable : 4996) //отключает Ошибку deprecate. Возникает, когда используется устаревшая функци
@@ -23,10 +24,23 @@ processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #pragma comment (lib, "shlwapi.lib")
 
 
+
+
 typedef BOOL(__stdcall *LPSEARCHFUNC)(LPCTSTR lpszFileName, const LPWIN32_FILE_ATTRIBUTE_DATA lpFileAttributeData, LPVOID lpvParam);
 BOOL Copy(LPCTSTR lpszFileName, const LPWIN32_FILE_ATTRIBUTE_DATA lpFileAttributeData, LPVOID lpvParam);
 BOOL FileSearch(LPCTSTR lpszFileName, LPCTSTR path, LPSEARCHFUNC lpSearchFunc, LPVOID lpvParam);
 BOOL FileOperation(LPCTSTR lpszFileName, LPCTSTR lpTargetDirectory, LPSEARCHFUNC lpFunc);
+
+// процедура диалогового окна 
+INT_PTR CALLBACK DialogProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+// обработчик сообщения WM_INITDIALOG
+BOOL Dialog_OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam);
+// обработчик сообщения WM_CLOSE
+void Dialog_OnClose(HWND hwnd);
+// обработчик сообщения WM_COMMAND
+void Dialog_OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify);
+
 
 
 BOOL Copy(LPCTSTR lpszFileName, const LPWIN32_FILE_ATTRIBUTE_DATA lpFileAttributeData, LPVOID lpvParam)
@@ -54,7 +68,6 @@ BOOL Copy(LPCTSTR lpszFileName, const LPWIN32_FILE_ATTRIBUTE_DATA lpFileAttribut
 	return CopyFile(lpszFileName, szNewFileName, FALSE);
 }
 
-
 BOOL FileSearch(LPCTSTR lpszFileName, LPCTSTR path, LPSEARCHFUNC lpSearchFunc, LPVOID lpvParam)
 {
 	WIN32_FIND_DATA ffd;
@@ -76,8 +89,7 @@ BOOL FileSearch(LPCTSTR lpszFileName, LPCTSTR path, LPSEARCHFUNC lpSearchFunc, L
 	}
 	do
 	{
-		if ((0 == lstrcmp(ffd.cFileName, L".")) ||
-			(0 == lstrcmp(ffd.cFileName, L"..")))
+		if ((0 == lstrcmp(ffd.cFileName, L".")) || (0 == lstrcmp(ffd.cFileName, L"..")))//пропускаем
 		{
 			continue;
 		}
@@ -104,13 +116,13 @@ BOOL FileOperation(LPCTSTR lpszFileName, LPCTSTR lpTargetDirectory, LPSEARCHFUNC
 		if (INVALID_FILE_ATTRIBUTES == dwFileAttributes) // (!) ошибка
 		{
 			return FALSE;
-		} // if
+		} 
 		else if ((dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) == 0) // (!) не является каталогом
 		{
 			SetLastError(ERROR_PATH_NOT_FOUND);
 			return FALSE;
-		} // if
-	} // if
+		} 
+	} 
 
 	WIN32_FILE_ATTRIBUTE_DATA fad;
 
@@ -119,79 +131,115 @@ BOOL FileOperation(LPCTSTR lpszFileName, LPCTSTR lpTargetDirectory, LPSEARCHFUNC
 
 	if (FALSE != bRet)
 	{
-		// выполняем операцию с файлом/каталогом
 		bRet = lpFunc(lpszFileName, &fad, (LPVOID)lpTargetDirectory);
-	} // if
+	} 
 
 	return bRet;
-} // FileOperation
+} 
 
 
-int _tmain(int argc, LPTSTR argv[])
+int WINAPI _wWinMain(HINSTANCE hInstance,	HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow)
 {
+	//WNDCLASSEX wcex = { sizeof(WNDCLASSEX) };
+
+	//wcex.style = CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS;
+	//wcex.lpfnWndProc = MainWindowProc; // оконная процедура
+	//wcex.hInstance = hInstance;
+	//wcex.lpszClassName = TEXT("MainWindowClass"); // имя класса
+	//wcex.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	//if (0 == RegisterClassEx(&wcex)) // регистрируем класс
+	//{
+	//	return -1; // завершаем работу приложения
+	//}
+
+	LoadLibrary(TEXT("ComCtl32.dll"));//для элементов общего пользования
+
+	HWND hwnd = CreateWindowEx(0, TEXT("MainWindowClass"), TEXT("Process"), WS_OVERLAPPEDWINDOW,
+		0, 0,100, 100, NULL, NULL, hInstance, NULL);
+	HWND hDlg = CreateDialog(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), hwnd, DialogProc);
+
+	MSG  msg;
+	BOOL Ret;
+
+	for (;;)
+	{
+
+		// извлекаем сообщение из очереди
+		Ret = GetMessage(&msg, NULL, 0, 0);
+		if (Ret == FALSE)
+		{
+			break; // получено WM_QUIT, выход из цикла
+		}
+		else if (!IsDialogMessage(hDlg, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+	return (int)msg.wParam;
+
+
+	//DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DialogProc, 0);
+
+	/*
 	_tsetlocale(LC_ALL, TEXT(""));
 	BOOL bRet = FileOperation(L"C:\\test\\libr1", L"C:\\test\\libr", Copy);
 	if (FALSE != bRet) _tprintf(TEXT("> Успешно!\n"));
 		else _tprintf(TEXT("> Ошибка: %d\n"), GetLastError());
-	//} // if
-
-} // _tmain
-
-
+	*/
+	return 0;
+} 
 
 
-//
-//#include <windows.h>
-//
-//void Copy(LPCTSTR szInDirName, LPCTSTR szOutDirName, bool flag = false)
-//{
-//	WIN32_FIND_DATA ffd;
-//	HANDLE hFind;
-//
-//	TCHAR szFind[MAX_PATH + 1];
-//	TCHAR szInFileName[MAX_PATH + 1];
-//	TCHAR szOutFileName[MAX_PATH + 1];
-//
-//	lstrcpy(szFind, szInDirName);
-//	lstrcat(szFind, L"\\*.*"); //ищем файлы с любым именем и рысширением
-//
-//	hFind = FindFirstFile(szFind, &ffd);
-//
-//	do
-//	{
-//		//Формируем полный путь (источник)
-//		lstrcpy(szInFileName, szInDirName);
-//		lstrcat(szInFileName, L"\\");
-//		lstrcat(szInFileName, ffd.cFileName);
-//
-//		//Формируем полный путь (результат)
-//		lstrcpy(szOutFileName, szOutDirName);
-//		lstrcat(szOutFileName, L"\\");
-//		lstrcat(szOutFileName, ffd.cFileName);
-//
-//		if (flag) //если flag == true, то копируем и папки
-//		{
-//			if (ffd.dwFileAttributes & 0x00000010)
-//			{
-//				if (lstrcmp(ffd.cFileName, L".") == 0 ||
-//					lstrcmp(ffd.cFileName, L"..") == 0) continue;
-//
-//				CreateDirectory(szOutFileName, NULL);
-//				Copy(szInFileName, szOutFileName);
-//			}
-//		} //иначе пропускаем папки
-//		else
-//			if (ffd.dwFileAttributes & 0x00000010) continue;
-//
-//		CopyFile(szInFileName, szOutFileName, TRUE);
-//	} while (FindNextFile(hFind, &ffd));
-//
-//	FindClose(hFind);
-//}
-//
-//int main()
-//{
-//	//В конце пути к папкам не нужно добавлять "\"
-//	Copy(L"C:\\test\\libr1", L"C:\\test\\libr", true); //если нужно скопировать и поддиректории
-//	//Copy(L"C:\\test\\libr1", L"C:\\test\\libr"); //если нужно скопировать только файлы
-//}
+INT_PTR CALLBACK DialogProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+	{
+		BOOL bRet = HANDLE_WM_INITDIALOG(hwndDlg, wParam, lParam, Dialog_OnInitDialog);
+		return SetDlgMsgResult(hwndDlg, uMsg, bRet);
+	}
+
+	case WM_CLOSE:
+		HANDLE_WM_CLOSE(hwndDlg, wParam, lParam, Dialog_OnClose);
+		return TRUE;
+
+	case WM_COMMAND:
+		HANDLE_WM_COMMAND(hwndDlg, wParam, lParam, Dialog_OnCommand);
+		return TRUE;
+	} // switch
+
+	return FALSE;
+} // DialogProc
+
+// ----------------------------------------------------------------------------------------------
+BOOL Dialog_OnInitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam)
+{
+	return TRUE;
+} 
+
+void Dialog_OnClose(HWND hwnd)
+{
+	EndDialog(hwnd, IDCLOSE);
+} 
+
+void Dialog_OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
+{
+	switch (id)
+	{
+	case IDOK:
+	{
+
+	}
+	break;
+
+	case IDCANCEL:
+	{
+		// завершаем работу диалогового окна
+		EndDialog(hwnd, IDCANCEL);
+	}
+	break;
+	} 
+} 
