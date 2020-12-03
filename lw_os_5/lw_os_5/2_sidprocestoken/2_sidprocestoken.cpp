64f53c4eca576fa0fa62746f0761689510806484
SofiFairyTell <58175476+SofiFairyTell@users.noreply.github.com>
2020-12-04 02:24:14 +0300

    Рабочий прототип части 2. Выводит список процессов, группы пользователей, пользователя, SID, и привилегии

--- lw_os_5/lw_os_5/2_sidprocestoken/2_sidprocestoken.cpp
+++ lw_os_5/lw_os_5/2_sidprocestoken/2_sidprocestoken.cpp
@@ -156,32 +156,32 @@ BOOL OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct)
 
 	//список для перечисления групп
 	CreateWindowEx(0, TEXT("Static"), TEXT("Группы"), WS_CHILD | WS_VISIBLE | SS_SIMPLE,
-		420, 10, 400, 200, hwnd, NULL, lpCreateStruct->hInstance, NULL);
+		420, 10, 300, 20, hwnd, NULL, lpCreateStruct->hInstance, NULL);
 
 	CreateWindowEx(0, TEXT("ListBox"), TEXT(""), WS_CHILD | WS_VISIBLE | WS_HSCROLL | WS_VSCROLL | WS_BORDER,
-		420, 30, 50, 30, hwnd, (HMENU)IDC_LB_GROUPS, lpCreateStruct->hInstance, NULL);
+		420, 30, 300, 300, hwnd, (HMENU)IDC_LB_GROUPS, lpCreateStruct->hInstance, NULL);
 
 	
 	//Поле "Пользователь"
 	CreateWindowEx(0, TEXT("Static"), TEXT("Пользователь:"), WS_CHILD | WS_VISIBLE | SS_SIMPLE,
-		570, 30, 100, 20, hwnd, NULL, lpCreateStruct->hInstance, NULL);
+		420, 330, 200, 20, hwnd, NULL, lpCreateStruct->hInstance, NULL);
 
 	CreateWindowEx(0, TEXT("Edit"), TEXT(""), WS_CHILD | WS_VISIBLE | ES_LEFT | ES_READONLY | ES_AUTOHSCROLL,
-		570, 130, 100, 20, hwnd, (HMENU)IDC_EDIT_ACCOUNT, lpCreateStruct->hInstance, NULL);
-/*
+		630, 330, 400, 20, hwnd, (HMENU)IDC_EDIT_ACCOUNT, lpCreateStruct->hInstance, NULL);
+
 	// Поле "SID"
 	CreateWindowEx(0, TEXT("Static"), TEXT("SID:"), WS_CHILD | WS_VISIBLE | SS_SIMPLE,
-		420, 50, 110, 20, hwnd, NULL, lpCreateStruct->hInstance, NULL);
+		420, 380, 200, 20, hwnd, NULL, lpCreateStruct->hInstance, NULL);
 
 	CreateWindowEx(0, TEXT("Edit"), TEXT(""), WS_CHILD | WS_VISIBLE | ES_LEFT | ES_READONLY | ES_AUTOHSCROLL,
-		570, 50, 400, 20, hwnd, (HMENU)IDC_EDIT_SID, lpCreateStruct->hInstance, NULL);
+		630, 380, 400, 20, hwnd, (HMENU)IDC_EDIT_SID, lpCreateStruct->hInstance, NULL);
 
 	//список для перечисления привилегий
 	CreateWindowEx(0, TEXT("Static"), TEXT("Привилегии:"), WS_CHILD | WS_VISIBLE | SS_SIMPLE,
-		420, 310, 400, 20, hwnd, NULL, lpCreateStruct->hInstance, NULL);
+		730, 10, 300, 20, hwnd, NULL, lpCreateStruct->hInstance, NULL);
 
 	CreateWindowEx(0, TEXT("ListBox"), TEXT(""), WS_CHILD | WS_VISIBLE | LBS_STANDARD,
-		420, 330, 400, 200, hwnd, (HMENU)IDC_LB_PRIVILEGES, lpCreateStruct->hInstance, NULL);
+		730, 30, 300, 300, hwnd, (HMENU)IDC_LB_PRIVILEGES, lpCreateStruct->hInstance, NULL);
 
 	/*
 	// Кнопки Вкл/Выкл
@@ -193,9 +193,10 @@ BOOL OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct)
 		530, 535, 110, 30, hwnd, (HMENU)IDC_BUTTON_PRIVILEGE_DISABLE, lpCreateStruct->hInstance, NULL);
 	EnableWindow(hwndCtl, FALSE);	// отключение "Выключить"
 	*/
+
 	//ComboBox для привелегий
 	hwndCtl = CreateWindowEx(0, TEXT("ComboBox"), TEXT(""), WS_CHILD | WS_VISIBLE | CBS_DROPDOWNLIST,
-		420, 580, 400, 30, hwnd, (HMENU)IDC_PRIVILEGES, lpCreateStruct->hInstance, NULL);
+		420, 430, 300, 200, hwnd, (HMENU)IDC_PRIVILEGES, lpCreateStruct->hInstance, NULL);
 
 	constexpr LPCTSTR Privilege[5] =
 	{
@@ -230,19 +231,19 @@ void OnSize(HWND hwnd, UINT state, int cx, int cy)
 		// изменяем высоту списка для перечисления процессов
 		MoveWindow(GetDlgItem(hwnd, IDC_LB_PROCESSES), 10, 30, 400, cy - 40, TRUE);
 
-		// изменяем высоту списка для перечисления ГРУПП
-		MoveWindow(GetDlgItem(hwnd, IDC_LB_GROUPS), 420, 30, 250, cy - 40, TRUE);
+		//// изменяем высоту списка для перечисления ГРУПП
+		//MoveWindow(GetDlgItem(hwnd, IDC_LB_GROUPS), 420, 30, 250, cy - 40, TRUE);
 
-		// изменяем ширину списка привилегий
-		MoveWindow(GetDlgItem(hwnd, IDC_LB_PRIVILEGES), 420, 330, cx - 430, 200, TRUE);
+		//// изменяем ширину списка привилегий
+		//MoveWindow(GetDlgItem(hwnd, IDC_LB_PRIVILEGES), 420, 330, cx - 430, 200, TRUE);
 
-		// изменяем ширину поля "Пользователь"
-		MoveWindow(GetDlgItem(hwnd, IDC_EDIT_ACCOUNT), 570, 30, 100, 20, TRUE);
-		// изменяем ширину поля "SID"
-		MoveWindow(GetDlgItem(hwnd, IDC_EDIT_SID), 570, 50, cx - 580, 20, TRUE);
+		//// изменяем ширину поля "Пользователь"
+		//MoveWindow(GetDlgItem(hwnd, IDC_EDIT_ACCOUNT), 570, 30, 100, 20, TRUE);
+		//// изменяем ширину поля "SID"
+		//MoveWindow(GetDlgItem(hwnd, IDC_EDIT_SID), 570, 50, cx - 580, 20, TRUE);
 
-		// изменяем ширину выпадающего списка "Привилегии"
-		MoveWindow(GetDlgItem(hwnd, IDC_PRIVILEGES), 420, 580, cx - 430, 30, TRUE);
+		//// изменяем ширину выпадающего списка "Привилегии"
+		//MoveWindow(GetDlgItem(hwnd, IDC_PRIVILEGES), 420, 580, cx - 430, 30, TRUE);
 
 	}
 }
@@ -269,9 +270,6 @@ void OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
 			HWND hwndListPrivileges = GetDlgItem(hwnd, IDC_LB_PRIVILEGES);
 			ListBox_ResetContent(hwndListPrivileges);// очистим список  привилегий
 
-			/*Кнопки*/
-			EnableWindow(GetDlgItem(hwnd, IDC_BUTTON_PRIVILEGE_ENABLE), FALSE);      // отключим кнопку "Включить"
-			EnableWindow(GetDlgItem(hwnd, IDC_BUTTON_PRIVILEGE_DISABLE), FALSE);	// отключим кнопку "Выключить"
 
 			int iItem = ListBox_GetCurSel(hwndCtl);//номер выделенной строки процесса
 			if (iItem != -1)
@@ -304,14 +302,14 @@ void OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
 								LocalFree(Name), Name = NULL;
 							}
 						
-							LocalFree(psid), psid = NULL;
+							//LocalFree(psid), psid = NULL;
 						}
 					ToLB_LoadTokenGroup(hwndListGroups, token);
 					ToLB_LoadTokenPrivil(hwndListPrivileges,token);
 					CloseHandle(token);
 				}
-			}//if
-		}//if
+			}
+		}
 	} break;
 
 	case IDC_LB_PRIVILEGES: // Обновление списка процессов
@@ -398,158 +396,6 @@ void OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
 	}
 }
 
-#pragma region Dialog Box for change priority 
-INT_PTR CALLBACK DialProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
-	switch (uMsg)
-	{
-	case WM_INITDIALOG:
-	{
-		BOOL bRet = HANDLE_WM_INITDIALOG(hwndDlg, wParam, lParam, Dialog_InitDialog);
-		return SetDlgMsgResult(hwndDlg, uMsg, bRet);
-	}
-
-	case WM_CLOSE:
-		HANDLE_WM_CLOSE(hwndDlg, wParam, lParam, Dialog_Close);
-		return TRUE;
-
-	case WM_COMMAND:
-		HANDLE_WM_COMMAND(hwndDlg, wParam, lParam, Dialog_Command);
-		return TRUE;
-	}
-
-	return FALSE;
-}
-
-// ------------------------------------------------------------------------------------------------
-BOOL Dialog_InitDialog(HWND hwnd, HWND hwndFocus, LPARAM lParam)
-{
-	HWND hwndCtl;
-	hwndCtl = GetDlgItem(hwnd, IDC_COMBO_PRIOR_CLASS);
-
-	/*Классы приоритета процесса*/
-	constexpr LPCTSTR PriorityClassesArr[6] = {
-		TEXT("Реального времени"),
-		TEXT("Высокий"),
-		TEXT("Выше среднего"),
-		TEXT("Средний"),
-		TEXT("Ниже среднего"),
-		TEXT("Низкий")
-	};
-
-	constexpr UINT PriorityClasses[6] = {
-		REALTIME_PRIORITY_CLASS,
-		HIGH_PRIORITY_CLASS,
-		ABOVE_NORMAL_PRIORITY_CLASS,
-		NORMAL_PRIORITY_CLASS,
-		BELOW_NORMAL_PRIORITY_CLASS,
-		IDLE_PRIORITY_CLASS
-	};
-
-	UINT PriorityClass = GetPriorityClass(GetCurrentProcess());
-
-
-	for (int i = 0; i < _countof(PriorityClasses); ++i)
-	{
-		int iItem = ComboBox_AddString(hwndCtl, PriorityClassesArr[i]);
-		ComboBox_SetItemData(hwndCtl, iItem, PriorityClasses[i]);
-
-		if (PriorityClasses[i] == PriorityClass)
-		{
-			ComboBox_SetCurSel(hwndCtl, iItem);
-		}
-	}
-
-	hwndCtl = GetDlgItem(hwnd, IDC_COMBO_PRIOR);
-	/*Относительные приоритеты потока*/
-	constexpr LPCTSTR PrioritiesArr[7] = {
-		TEXT("Критичный по времени"),
-		TEXT("Максимальный"),
-		TEXT("Выше среднего"),
-		TEXT("Средний"),
-		TEXT("Ниже среднего"),
-		TEXT("Минимальный"),
-		TEXT("Простаивающий")
-	};
-
-	constexpr UINT Priorities[7] = {
-		THREAD_PRIORITY_TIME_CRITICAL,
-		THREAD_PRIORITY_HIGHEST,
-		THREAD_PRIORITY_ABOVE_NORMAL,
-		THREAD_PRIORITY_NORMAL,
-		THREAD_PRIORITY_BELOW_NORMAL,
-		THREAD_PRIORITY_LOWEST,
-		THREAD_PRIORITY_IDLE
-	};
-
-	UINT Priority = GetThreadPriority(GetCurrentThread());
-
-	for (int i = 0; i < _countof(Priorities); ++i)
-	{
-		int SelItem = ComboBox_AddString(hwndCtl, PrioritiesArr[i]);
-		ComboBox_SetItemData(hwndCtl, SelItem, Priorities[i]);
-
-		if (Priorities[i] == Priority)
-		{
-			ComboBox_SetCurSel(hwndCtl, SelItem);
-		}
-	}
-
-	return TRUE;
-}
-
-
-void Dialog_Close(HWND hwnd)
-{
-	EndDialog(hwnd, IDCLOSE);
-}
-
-void Dialog_Command(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
-{
-	switch (id)
-	{
-	case IDOK:
-	{
-		HWND hwndCtl;
-		int SelItem;
-
-		hwndCtl = GetDlgItem(hwnd, IDC_COMBO_PRIOR_CLASS);
-
-		/*ПОЛУЧЕНИЕ КЛАССА ПРИОРИТЕТА*/
-
-		SelItem = ComboBox_GetCurSel(hwndCtl);
-		UINT PriorityClass = (SelItem != -1) ? (UINT)ComboBox_GetItemData(hwndCtl, SelItem) : NORMAL_PRIORITY_CLASS;
-
-		/*ЗАДАНИЕ КЛАССА ПРИОРИТЕТА*/
-
-		SetPriorityClass(GetCurrentProcess(), PriorityClass);
-
-		/*ИЗМЕНЕНИЕ ОТНОСИТЕЛЬНОГО ПРИОРИТЕТА ДЛЯ ГЛАВНОГО ПОТОКА */
-
-		hwndCtl = GetDlgItem(hwnd, IDC_COMBO_PRIOR);
-
-		/*ПОЛУЧЕНИЕ ОТНОСИТЕЛЬНОГО ПРИОРИТЕТА ДЛЯ ГЛАВНОГО ПОТОКА*/
-		SelItem = ComboBox_GetCurSel(hwndCtl);
-		UINT Priority;
-		if (SelItem != -1)
-			Priority = (UINT)ComboBox_GetItemData(hwndCtl, SelItem);
-		else Priority = THREAD_PRIORITY_NORMAL;
-
-		//UINT Priority = (SelItem != -1) ? (UINT)ComboBox_GetItemData(hwndCtl, SelItem) : THREAD_PRIORITY_NORMAL; //тернарная операция
-
-		/*ЗАДАНИЕ ОТНОСИТЕЛЬНОГО ПРИОРИТЕТА*/
-		SetThreadPriority(GetCurrentThread(), Priority);
-
-		EndDialog(hwnd, IDOK);
-	}
-	break;
-
-	case IDCANCEL:
-		EndDialog(hwnd, IDCANCEL);
-		break;
-	}
-}
-#pragma endregion
 
 
 
@@ -593,193 +439,7 @@ void ToLB_LoadProcesses(HWND hwndCtl)
 	}
 }
 
-void ToLB_LoadModules(HWND hwndCtl, DWORD PID)
-{
-	ListBox_ResetContent(hwndCtl);
 
-	HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, PID);
-
-	if (NULL != hProcess)
-	{
-		/*определение размера списка модулей*/
-		DWORD cMod = 0;
-		EnumProcessModulesEx(hProcess, NULL, 0, &cMod, LIST_MODULES_ALL);
-
-		DWORD NcMod = cMod / sizeof(HMODULE);//столько будет модулей
-
-		HMODULE *hModule = new HMODULE[NcMod];//столько потребуется памяти
-
-		// получаем список модулей
-		cMod = NcMod * sizeof(HMODULE);
-		BOOL bRet = EnumProcessModulesEx(hProcess, hModule, cMod, &cMod, LIST_MODULES_ALL);
-
-		if (FALSE != bRet)
-		{
-			TCHAR ModuleName[MAX_PATH];
-
-			for (DWORD i = 0; i < NcMod; ++i)
-			{
-				bRet = GetModuleFileNameEx(hProcess, hModule[i], ModuleName, MAX_PATH);
-				if (FALSE != bRet) ListBox_AddString(hwndCtl, ModuleName); // добавляем в список новую строку
-			}
-		}
-
-		/*Освобождение ресурсов*/
-		delete[]hModule;
-		CloseHandle(hProcess);
-	}
-}
-
-void ToLB_LoadProcessesInJob(HWND hwndCtl, HANDLE hJob)
-{
-
-	ListBox_ResetContent(hwndCtl);
-
-	DWORD PIDarray[1024]; //массив для всех идентификаторов процессов
-	DWORD cbNeeded = 0;//размер блока памяти с идентификаторами
-	BOOL bRet = EnumProcessesInJob(hJob, PIDarray, sizeof(PIDarray), &cbNeeded);
-
-	if (FALSE != bRet)
-	{
-		TCHAR ProcessName[MAX_PATH];//имя процесса
-		TCHAR szBuffer[300];//строка в которую запишем имя процесса и его номер
-
-		for (DWORD i = 0,
-			n = cbNeeded / sizeof(DWORD); i < n; ++i)
-		{
-			DWORD PID = PIDarray[i], cch = 0;
-
-			HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, PID);
-
-			if (NULL != hProcess)
-			{
-
-				cch = GetModuleBaseName(hProcess, NULL, ProcessName, _countof(ProcessName));// получаем имя главного модуля процесса
-				CloseHandle(hProcess); // закрываем объект ядра
-			} // if
-
-			if (0 == cch)
-				StringCchCopy(ProcessName, MAX_PATH, TEXT("Неизвестный процесс"));
-
-			StringCchPrintf(szBuffer, _countof(szBuffer), TEXT("%s (PID: %u)"), ProcessName, PID);// формируем строку для списка
-
-			int iItem = ListBox_AddString(hwndCtl, szBuffer);
-
-			ListBox_SetItemData(hwndCtl, iItem, PID);// сохраняем в новой строке идентификатор процесса
-		}
-	}
-}
-
-
-BOOL EnumProcessesInJob(HANDLE hJob, DWORD* lpPID, DWORD cb, LPDWORD lpcbNeeded)
-{
-
-	DWORD nCount = cb / sizeof(ULONG_PTR);//столько max процессов будет в буфере
-
-	if (NULL != lpPID && nCount > 0)
-	{
-		DWORD jobProcessStructSize = sizeof(JOBOBJECT_BASIC_PROCESS_ID_LIST) + sizeof(ULONG_PTR) * 1024;//блок памяти для структуры
-		JOBOBJECT_BASIC_PROCESS_ID_LIST* jobProcessIdList = static_cast<JOBOBJECT_BASIC_PROCESS_ID_LIST*>(malloc(jobProcessStructSize));
-
-		if (NULL != jobProcessIdList)
-		{
-			jobProcessIdList->NumberOfAssignedProcesses = nCount;// MAX число процессов, на которое расчитана выделенная память
-			// запрашиваем список идентификаторов процессов
-			BOOL bRet = QueryInformationJobObject(hJob, JobObjectBasicProcessIdList, jobProcessIdList, jobProcessStructSize, NULL);
-
-			if (FALSE != bRet)
-			{
-				nCount = jobProcessIdList->NumberOfProcessIdsInList;// определяем количество идетификаторов
-				CopyMemory(lpPID, jobProcessIdList->ProcessIdList, nCount * sizeof(ULONG_PTR));//copies a block of memory from one location to another
-
-				if (NULL != lpcbNeeded)
-					*lpcbNeeded = nCount * sizeof(ULONG_PTR);//размер блока памяти с идентификаторами
-			}
-
-			free(jobProcessIdList); // освобождаем память
-			return bRet;
-		}
-	}
-
-	return FALSE;
-}
-
-BOOL StartGroupProcessesAsJob(HANDLE hjob, LPCTSTR lpszCmdLine[], DWORD nCount, BOOL bInheritHandles, DWORD CreationFlags)
-{
-
-	BOOL bInJob = FALSE;
-	IsProcessInJob(GetCurrentProcess(), NULL, &bInJob);// определим, включен ли вызывающий процесс в задание
-
-	JOBOBJECT_BASIC_LIMIT_INFORMATION jobli = { 0 };//базовые ограничения
-
-	QueryInformationJobObject(NULL, JobObjectBasicLimitInformation, &jobli, sizeof(jobli), NULL);
-	JOBOBJECT_BASIC_UI_RESTRICTIONS jobuir;
-	//процессу запрещено чтение из буфера обмена
-	jobuir.UIRestrictionsClass |= JOB_OBJECT_UILIMIT_READCLIPBOARD;
-
-	SetInformationJobObject(hjob, JobObjectBasicUIRestrictions, &jobuir, sizeof(jobuir));
-
-	//ограничения если вызывающий процесс связан с заданием
-	//первый флаг - сообщаем что новый процесс может выполнятся вне задания
-	//второй флаг - сообщаем, что новый процесс не является частью задания
-
-	DWORD dwLimitMask = JOB_OBJECT_LIMIT_BREAKAWAY_OK | JOB_OBJECT_LIMIT_SILENT_BREAKAWAY_OK;
-
-	if ((jobli.LimitFlags & dwLimitMask) == 0)
-	{
-		/* все порожденные процессы   автоматически включаются в задание */
-		return FALSE;
-	}
-
-	// порождаем процессы...
-	STARTUPINFO si = { sizeof(STARTUPINFO) };
-	PROCESS_INFORMATION pi = { 0 };
-	TCHAR szCmdLine[MAX_PATH];
-
-
-	for (DWORD i = 0; i < nCount; ++i)
-	{
-		StringCchCopy(szCmdLine, MAX_PATH, lpszCmdLine[i]);
-		// порождаем новый процесс,
-		// приостанавливая работу его главного потока
-		BOOL bRet = CreateProcess(NULL, szCmdLine, NULL, NULL,
-			bInheritHandles, CreationFlags | CREATE_SUSPENDED | CREATE_BREAKAWAY_FROM_JOB, NULL, NULL, &si, &pi);
-
-		if (FALSE != bRet)
-		{
-			//Дочерние процессы, порождаемые этим процессом станут частью этого задания автоматически
-			AssignProcessToJobObject(hjob, pi.hProcess);// добавляем новый процесс в задание
-
-			ResumeThread(pi.hThread);// возобновляем работу потока нового процесса
-
-			CloseHandle(pi.hThread);// закрывает дескриптор потока нового процесса
-
-			CloseHandle(pi.hProcess);// закрывает дескриптор нового процесса
-		}
-	}
-	return TRUE;
-}
-
-BOOL WaitProcessById(DWORD PID, DWORD WAITTIME, LPDWORD lpExitCode)
-{
-	HANDLE hProcess = OpenProcess(SYNCHRONIZE | PROCESS_QUERY_INFORMATION, FALSE, PID);// открываем процесс
-
-	if (NULL == hProcess)
-	{
-		return FALSE;
-	}
-
-	WaitForSingleObject(hProcess, WAITTIME);// ожидаем завершения процесса
-
-	if (NULL != lpExitCode)
-	{
-		GetExitCodeProcess(hProcess, lpExitCode);// получим код завершения процесса
-	}
-
-	CloseHandle(hProcess);// закрываем дескриптор процесса
-
-	return TRUE;
-}
 
 
 
@@ -797,46 +457,8 @@ HANDLE OpenProcessTokenPID(UINT PID, DWORD AccessRIGHT)
 	return token;
 }
 
-BOOL GetTokenUser(HANDLE token, PSID *psid)
-{
-	DWORD tkSize;
-	TOKEN_USER *tkUser;
-	DWORD sidLeng;
-	BOOL RetRes = FALSE;
 
-	GetTokenInformation(token, TokenUser, NULL, 0, &tkSize);//определение размера
-	tkUser = (TOKEN_USER*)malloc(tkSize); //выделение блока памяти
-	if (tkUser == NULL)
-	{
-		return E_OUTOFMEMORY; 
-	}
-	
-	if (GetTokenInformation(token, TokenUser, tkUser, tkSize, &tkSize))
-	{
-		sidLeng = GetLengthSid(tkUser->User.Sid);
-		PSID npsid = NULL;
-		npsid = (PSID)malloc(sidLeng);
-		if (npsid == NULL)
-		{
-			return E_OUTOFMEMORY;
-		}
-
-		RetRes = CopySid(sidLeng, npsid, tkUser->User.Sid); //копируем опеределнный SID перед возвратом
-
-		if (RetRes != FALSE)
-		{
-			*psid = npsid;
-		}
-		else
-		{
-			LocalFree(npsid);
-			//free(tkUser)
-		}
-	}
-
-	return RetRes;
-}
-
+/*Получение имени аккаунта по его SID*/
 BOOL GetAccountName_W(PSID psid, LPWSTR* AccountName)
 {
 	BOOL RetRes = FALSE;
@@ -851,9 +473,12 @@ BOOL GetAccountName_W(PSID psid, LPWSTR* AccountName)
 	}
 
 	LookupAccountSid(NULL, psid, NULL, &cch, NULL, &cchRefDomainName, NULL);//определим размеры буферов
-	DWORD cb = (cch + cchRefDomainName) * sizeof(WCHAR);
+
+	DWORD cb = (cch + cchRefDomainName) * sizeof(TCHAR);
+
 	if (cb > 0)
 	{
+
 		Name = (LPWSTR)LocalAlloc(LMEM_FIXED, cb);//выделение памяти из локальной кучи процесса
 	}
 
@@ -872,14 +497,14 @@ BOOL GetAccountName_W(PSID psid, LPWSTR* AccountName)
 			}
 			else
 			{
-				std::wcscpy(Name, Name + 1);//копирование для вовзрата в основую функциию
+				StringCbCopy(Name, cb, Name + 1);//копирование для вовзрата в основую функциию
 			}
 		}
 		*AccountName = Name; //вернем полученнное имя в программу
 	}
 	else
 	{
-		ConvertSidToStringSidW(psid, AccountName);//если не получилось получить имя, то вернем SID
+		ConvertSidToStringSid(psid, AccountName);//если не получилось получить имя, то вернем SID
 		if (Name != NULL)
 		{
 			LocalFree(Name);
@@ -943,7 +568,7 @@ void ToLB_LoadTokenPrivil(HWND hwndListPrivileges, HANDLE token)
 }
 
 
-
+/*Токены*/
 BOOL GetTokenGR(HANDLE token, PTOKEN_GROUPS *tk_groups)
 {
 	DWORD tkSize;
@@ -966,6 +591,46 @@ BOOL GetTokenGR(HANDLE token, PTOKEN_GROUPS *tk_groups)
 	return RetRes;
 }
 
+BOOL GetTokenUser(HANDLE token, PSID *psid)
+{
+	DWORD tkSize;
+	TOKEN_USER *tkUser;
+	DWORD sidLeng;
+	BOOL RetRes = FALSE;
+
+	GetTokenInformation(token, TokenUser, NULL, 0, &tkSize);//определение размера
+	tkUser = (TOKEN_USER*)malloc(tkSize); //выделение блока памяти
+	if (tkUser == NULL)
+	{
+		return E_OUTOFMEMORY; 
+	}
+	
+	if (GetTokenInformation(token, TokenUser, tkUser, tkSize, &tkSize))
+	{
+		sidLeng = GetLengthSid(tkUser->User.Sid);
+		PSID npsid = NULL;
+		npsid = (PSID)malloc(sidLeng);
+		if (npsid == NULL)
+		{
+			return E_OUTOFMEMORY;
+		}
+
+		RetRes = CopySid(sidLeng, npsid, tkUser->User.Sid); //копируем опеределнный SID перед возвратом
+
+		if (RetRes != FALSE)
+		{
+			*psid = npsid;
+		}
+		else
+		{
+			LocalFree(npsid);
+			//free(tkUser)
+		}
+	}
+
+	return RetRes;
+}
+
 BOOL GetTokenPR(HANDLE token, PTOKEN_PRIVILEGES *tk_PR)
 {
 	DWORD tkSize;
@@ -976,7 +641,7 @@ BOOL GetTokenPR(HANDLE token, PTOKEN_PRIVILEGES *tk_PR)
 
 	if (privil == NULL) return FALSE;
 
-	if (GetTokenInformation(token, TokenGroups, privil, tkSize, &tkSize))//получение списка привелегий в маркере доступа
+	if (GetTokenInformation(token, TokenPrivileges, privil, tkSize, &tkSize))//получение списка привелегий в маркере доступа
 	{
 		RetRes = TRUE;
 		*tk_PR = privil;
