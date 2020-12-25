8182aedd065f63bc617d3e61cdeadbe2d7a20fc4
SofiFairyTell <58175476+SofiFairyTell@users.noreply.github.com>
2020-12-25 11:41:48 +0300

    ADD BUTTON: CLEAR_ALL

--- lw_ict_4/lw_os_6/Source.cpp
+++ lw_ict_4/lw_os_6/Source.cpp
@@ -18,13 +18,13 @@
 
 #define IDC_RBUTTON_WM_SETTEXT      2004
 
-#define IDC_RBUTTON_CLIPBOARD       2006
+#define IDC_CLEAR_ALL					2006 //кнопка чтобы удалить весь отправленный текст
 
 #define IDC_RBUTTON_PIPE            2006
 #define MAX_TEXT                    1024
 
 //размеры текста с сообщением
-#define MAX_MESSAGE_SIZE            10
+#define MAX_MESSAGE_SIZE            255
 #define MAX_USERNAME_SIZE			20
 /*Дескрипторы*/
 HWND hwnd = NULL; // дескриптор главного окна
@@ -63,7 +63,10 @@ SOCKET sockets;
 sockaddr_in sockSin = { 0 };
 sockaddr_in sockSout = { 0 };
 sockaddr_in sockSoin = { 0 };
+
+//отправка сообщений
 void SendText(SLP_msg msg, LPCTSTR lpData, DWORD cbData, BOOL DataCopy);
+void StartChat(HWND hwnd, LPCTSTR Message);
 int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE, LPTSTR CmdLine, int CmdShow)
 {
 
@@ -90,7 +93,7 @@ int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE, LPTSTR CmdLine, int CmdShow
 
 	/*Создание главного файла и обработка ошибки */
 	hwnd = CreateWindowEx(0, TEXT("MainWindowProcess"), TEXT("Chat"),
-		WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, 0, 900, 500, NULL, NULL, hInstance, NULL);
+		WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, 0, 900, 600, NULL, NULL, hInstance, NULL);
 
 	if (hwnd == NULL)
 	{
@@ -127,7 +130,7 @@ LRESULT CALLBACK MainWindowProcess(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lP
 	{
 		HANDLE_MSG(hWnd, WM_CREATE, OnCreate);
 		HANDLE_MSG(hWnd, WM_DESTROY, OnDestroy);
-		//HANDLE_MSG(hWnd, WM_COMMAND, OnCommand);
+		HANDLE_MSG(hWnd, WM_COMMAND, OnCommand);
 	}
 	return DefWindowProc(hWnd, msg, wParam, lParam);
 }
@@ -143,6 +146,7 @@ BOOL PreTranslateMessage(LPMSG Msg)
 	//объявим структуру с пакетом
 	SLP_msg msg;
 	memset(msg.text, NULL, 10);
+	memset(msg.username, NULL, 20);
 	DWORD symbols, symb_user;  //количество символов в сообщении
 
 	if ((WM_KEYDOWN == Msg->message) && (VK_RETURN == Msg->wParam)) // нажата клавиша Enter
@@ -171,13 +175,9 @@ BOOL PreTranslateMessage(LPMSG Msg)
 					StringCchCat(msg.username,20, UserName);
 
 					MessageBox(hwnd, L"thread", L"tr", MB_OK);
-					//StringCchCat(UserName, _countof(UserName), _T(":"));
-					//StringCchCat(Message, _countof(Message), _T("\n\n"));
 
-					//StringCchCat(UserMessage, _countof(UserMessage), UserName);
-
-					//StringCchCat(UserMessage, _countof(UserMessage), Message);
-
+					StartChat(hwnd,Message);//отобрзим отправляемый текст у себя
+					
 					BOOL optval = TRUE;
 					int optlen = sizeof(optval);
 					int err = setsockopt(sockets, SOL_SOCKET, SO_BROADCAST, (char*)&optval, optlen);
@@ -185,6 +185,7 @@ BOOL PreTranslateMessage(LPMSG Msg)
 					sockSin.sin_port = htons(7581);
 					sockSin.sin_addr.s_addr = htonl(INADDR_BROADCAST);
 
+					
 
 					SendText(msg, Message, _tcslen(Message), FALSE);
 
@@ -206,11 +207,14 @@ BOOL OnCreate(HWND hwnd, LPCREATESTRUCT lpCreateStruct)
 		10, 10, 490, 250, hwnd, (HMENU)IDC_EDIT_MESSAGES, lpCreateStruct->hInstance, NULL);
 
 	//Для username
-	CreateWindowEx(0, TEXT("Static"), _T("Имя: "), WS_CHILD | WS_VISIBLE | SS_SIMPLE,
+	CreateWindowEx(0, TEXT("Static"), TEXT("User: "), WS_CHILD | WS_VISIBLE | SS_SIMPLE,
 		10, 270, 40, 40, hwnd, NULL, lpCreateStruct->hInstance, NULL);
 
 	CreateWindowEx(WS_EX_CLIENTEDGE, TEXT("Edit"), TEXT(""), WS_CHILD | WS_VISIBLE,
 		50, 270, 450, 40, hwnd, (HMENU)IDC_EDIT_USERNAME, lpCreateStruct->hInstance, NULL);
+
+	CreateWindowEx(0, TEXT("Button"), TEXT("Clear History"), WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 
+		530, 10, 200, 30, hwnd, (HMENU)IDC_CLEAR_ALL, lpCreateStruct->hInstance, NULL);
 
 	// создаём поле ввода
 	HWND hwndCtl = CreateWindowEx(WS_EX_CLIENTEDGE, TEXT("Edit"), TEXT(""), dwStyle,
@@ -250,7 +254,23 @@ void OnDestroy(HWND hwnd)
 	PostQuitMessage(0); // отправляем сообщение WM_QUIT
 }
 
+void OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
+{
+	if (BN_CLICKED == codeNotify)
+	{
+		HINSTANCE hInstance = GetWindowInstance(hwnd);
+		switch (id)
+		{
+		case IDC_CLEAR_ALL:
+		{
+			HWND hwndCtl = GetDlgItem(hwnd, IDC_EDIT_MESSAGES);		
+			Edit_SetText(hwndCtl, NULL);// очищаем поле ввода
+		}
+		break;
+		}
 
+	}
+}
 
 
 // ----------------------------------------------------------------------------------------------
@@ -284,7 +304,8 @@ void SendText(SLP_msg msg,LPCTSTR lpData, DWORD cbData, BOOL DataCopy)
 				
 void StartChat(HWND hwnd, LPCTSTR Message)
 {
-	MessageBox(hwnd, L"CHAT", L"tr", MB_OK);
+	MessageBox(hwnd, L"LET'S GO CHAT", L"tr", MB_OK);
+
 	HWND hwndCtl = GetDlgItem(hwnd, IDC_EDIT_MESSAGES);
 	Edit_SetSel(hwndCtl, Edit_GetTextLength(hwndCtl), -1);// устанавливаем курсор в конец поля вывода
 	Edit_ReplaceSel(hwndCtl, Message);// вставляем текст в поле вывода
@@ -293,20 +314,19 @@ void StartChat(HWND hwnd, LPCTSTR Message)
 unsigned __stdcall ThreadFunc(void* lParam)
 {
 	/*Переменные*/
-	WCHAR Message[MAX_MESSAGE_SIZE]; //сообщение
+	WCHAR Message[MAX_MESSAGE_SIZE] = _T(""); //сообщение
 	WCHAR UserName[MAX_USERNAME_SIZE] = _T("");//имя отправителя
 	WCHAR UserMessage[532] = _T("");
 
 	SLP_msg recived_msg = {0};
 	int socket_len  = sizeof(sockSout);
-	//WCHAR text[255] = L"";
-	//WCHAR show_text[255] = L"";
+
 	for (;;)
 	{
 		//Начнем прием сообщений через сокет
 		int result = recvfrom(sockets,(char*)&recived_msg,sizeof(recived_msg),NULL,(struct sockaddr*)&sockSout, &socket_len);
 		
-		MessageBox(hwnd, L"thread", L"tr", MB_OK);
+		//MessageBox(hwnd, L"START RECEVIENG MSG", L"tr", MB_OK);
 		if(result != SOCKET_ERROR)
 		{
 			int reseived_size = recived_msg.filelen/5;
@@ -320,7 +340,6 @@ unsigned __stdcall ThreadFunc(void* lParam)
 				{
 					for (int j = 0; j < 5; j++)
 					{
-						
 						Message[i+j] = recived_msg.text[j];
 					}
 				}
@@ -342,9 +361,7 @@ unsigned __stdcall ThreadFunc(void* lParam)
 
 					StringCchCat(UserMessage, _countof(UserMessage), Message);
 
-					MessageBox(hwnd, L"!!!!!", L"tr", MB_OK);
-
-					StartChat(hwnd, Message);
+					StartChat(hwnd, UserMessage);
 
 					memset(Message, NULL, 255);
 					memset(Message, NULL, 255);
